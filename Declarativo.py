df939bcfd1926de82ca3bb15c14981d95c8d1c92
Christian Rodriguez <crodriguezh@ulasalle.edu.pe>
2022-08-19 09:25:01 -0500

    Finalizado

---
+++ Declarativo.py
@@ -0,0 +1,6 @@
+Lista = [10,20,30,40,50]
+
+# Se obtiene la suma total de la lista ingresada
+
+total = sum(Lista)
+print(total) 
---
+++ Imperativo.py
@@ -0,0 +1,10 @@
+# Se ingresa una lista con numeros enteros
+
+Lista = [10,20,30,40,50]
+total = 0 
+
+# Se procesa la suma en un bucle (for)
+
+for a in Lista:
+     total += a
+print(total)
